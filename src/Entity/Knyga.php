ea5a3a31e538f69e8c2169667cfb1dc99610291d
Vilius Seputis <vilius951@gmail.com>
2025-01-20 18:32:50 +0200

    Initial commit with migration and first entity

---
+++ src/Entity/Knyga.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\KnygaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: KnygaRepository::class)]
+class Knyga
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $pavadinimas = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $autorius = null;
+
+    #[ORM\Column]
+    private ?int $isleidimo_metai = null;
+
+    #[ORM\Column(length: 13)]
+    private ?string $ISBN = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPavadinimas(): ?string
+    {
+        return $this->pavadinimas;
+    }
+
+    public function setPavadinimas(string $pavadinimas): static
+    {
+        $this->pavadinimas = $pavadinimas;
+
+        return $this;
+    }
+
+    public function getAutorius(): ?string
+    {
+        return $this->autorius;
+    }
+
+    public function setAutorius(string $autorius): static
+    {
+        $this->autorius = $autorius;
+
+        return $this;
+    }
+
+    public function getIsleidimoMetai(): ?int
+    {
+        return $this->isleidimo_metai;
+    }
+
+    public function setIsleidimoMetai(int $isleidimo_metai): static
+    {
+        $this->isleidimo_metai = $isleidimo_metai;
+
+        return $this;
+    }
+
+    public function getISBN(): ?string
+    {
+        return $this->ISBN;
+    }
+
+    public function setISBN(string $ISBN): static
+    {
+        $this->ISBN = $ISBN;
+
+        return $this;
+    }
+}
